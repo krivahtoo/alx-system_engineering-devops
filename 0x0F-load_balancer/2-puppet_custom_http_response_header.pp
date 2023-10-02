3a67532e89332ae01596eb7aecfa0e5439625901
Noah Too <krivahtoo@gmail.com>
2023-10-02 22:56:47 +0300

    Task 2. Add a custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,27 @@
+# setup nginx using puppet
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+
+file_line { 'redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+file_line { 'custom_response':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'try_files $uri $uri/ =404;',
+  line   => 'add_header X-Served-By $hostname;',
+}
