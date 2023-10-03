65b83ae746c988576a815a1ce31514c03016bee9
Noah Too <krivahtoo@gmail.com>
2023-10-03 21:52:59 +0300

    fix: nginx custom header config

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -22,6 +22,6 @@ file { '/var/www/html/index.html':
 file_line { 'custom_response':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'try_files $uri $uri/ =404;',
-  line   => 'add_header X-Served-By $hostname;',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname always;',
 }
